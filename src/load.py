32730df1f606448778bb24c34c05f1cfe09a05ae
bound <boundwhd@126.com>
2025-06-07 13:12:50 +0800

    fix a word error

--- src/load.py
+++ src/load.py
@@ -13,6 +13,13 @@ from transformers import AutoTokenizer
 from config import Qwen3Config
 from model import Qwen3ForCausalLM
 
+def check_device_availability(device: torch.device):
+    if device == "cuda":
+        return torch.cuda.is_available()
+    elif device == "mps":
+        return torch.backends.mps.is_available() # MPS backend availability check
+    return True
+
 def weight_load(
     model: Qwen3ForCausalLM, 
     checkpoint_path: str, 
@@ -51,12 +58,18 @@ def model_load(
     model_name: str,
     device: torch.device,
     checkpoint: str,
-):
+):  
+    if not check_device_availability(device):
+        print(f"Specified device ({device}) is not available. Switching to CPU.")
+        device = torch.device("cpu")
+    
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     config = Qwen3Config()
     model = Qwen3ForCausalLM(config=config).to(device=device, dtype=config.torch_type)
     print("Start loading model weight......")
     model = weight_load(model=model, checkpoint_path=checkpoint, device=device)
+    
+    print(f"Model is loaded on {device}.")
     return model, tokenizer
 
 
@@ -68,8 +81,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Export Qwen3 model weights to .pth file")
     parser.add_argument("--model_name", type=str, default="Qwen/Qwen3-0.6B",
                        help="HuggingFace model name or path (default: Qwen/Qwen3-0.6B)")
-    parser.add_argument("--output_file", type=str, default="qwen3_0.6B_weights.pth",
-                       help="Output .pth file path (default: qwen3_0.6B_weights.pth)")
+    parser.add_argument("--output_file", type=str, default="qwen3_0.6b_weights.pth",
+                       help="Output .pth file path (default: qwen3_0.6b_weights.pth)")
     args = parser.parse_args()
 
     tokenizer = AutoTokenizer.from_pretrained(args.model_name)
